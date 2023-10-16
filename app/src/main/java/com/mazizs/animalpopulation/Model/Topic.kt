a89d432eb012f0cf390171a988563630766724b9
Muh Aziz Sholih <90168755+MAzizSholih@users.noreply.github.com>
2023-10-16 12:06:45 +0700

    Topic.kt

--- app/src/main/java/com/mazizs/animalpopulation/Model/Topic.kt
+++ app/src/main/java/com/mazizs/animalpopulation/Model/Topic.kt
@@ -5,6 +5,6 @@ import androidx.annotation.StringRes
 
 data class Topic(
     @StringRes val name: Int,
-    val availableCourses: Int,
+    val availablePopulation: Int,
     @DrawableRes val imageRes: Int
 )
